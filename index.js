f412ba16d27ed36c41a83d1f6eedfcef9952963e
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-11-05 16:37:59 -0600

    login and register

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ import { PORT } from './config.js'
 import { UserRepository } from './user-repository.js';
 
 const app = express();
+
+app.set('view engine', 'ejs');
 // middleware
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello world!!!');
+  res.render('index');
 });
 
-app.post('/login', (req, res) => {
-  res.json({user: 'user'});
+app.post('/login', async(req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await UserRepository.login({username, password});
+    res.send({ user });
+  } catch (error) {
+    res.status(401).send({ error: error.message });
+  }
 });
-app.post('/register', (req, res) => {
+app.post('/register', async(req, res) => {
   const { username, password } = req.body;
 
   try {
-    const id = UserRepository.create({username, password});
+    const id = await UserRepository.create({username, password});
     res.send({ id });
   } catch (error) {
     // normally, it is not a good idea to send the error.message
--- user-repository.js
+++ user-repository.js
@@ -1,5 +1,9 @@
 import DBLocal from 'db-local';
-import crypto from 'crypto';
+import crypto from 'node:crypto';
+import bcrypt from 'bcrypt';
+
+import { SALT_ROUNDS } from './config.js';
+
 const { Schema } = new DBLocal({ path: './db' });
 
 const User = Schema('User', {
@@ -9,28 +13,54 @@ const User = Schema('User', {
 });
 
 // an interface with typescript would be useful
+// the body of the function may change but the params and the return must not
 export class UserRepository {
-  static create ({ username, password }) {
+  static async create ({ username, password }) {
     // optional: use zod for validations
-    if (typeof username !== 'string') throw new Error('username must be a string');
-    if (username.length < 3) throw new Error('username must be at least 3 characters long');
-
-    if (typeof password !== 'string') throw new Error('password must be a string');
-    if (password.length < 6) throw new Error('password must be at least 6 characters long');
+    Validation.username(username);
+    Validation.password(password);
 
     const user = User.findOne({ username });
     if (user) throw new Error('username already exists');
 
     const id = crypto.randomUUID();
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS); // hashSync blocks main thread
 
     User.create({
       _id: id,
       username,
-      password
+      password: hashedPassword
     }).save();
 
     return id;
 
   }
-  static login ({ username, password }) {}
+  static async login ({ username, password }) {
+    Validation.username(username);
+    Validation.password(password);
+
+    const user = User.findOne({ username });
+    if (!user) throw new Error('username does not exist');
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) throw new Error('password is invalid');
+
+    const { password: _, ...publicUser } = user;
+
+    return publicUser;
+
+  }
+}
+
+// DRY dont repeat yourself
+class Validation {
+  static username(username) {
+    if (typeof username !== 'string') throw new Error('username must be a string');
+    if (username.length < 3) throw new Error('username must be at least 3 characters long');
+  }
+
+  static password(password) {
+    if (typeof password !== 'string') throw new Error('password must be a string');
+    if (password.length < 6) throw new Error('password must be at least 6 characters long');
+  }
 }
