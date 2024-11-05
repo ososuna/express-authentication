7851fcb277a2aa2f4b331c698b9c2b457d2fdd0a
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-11-05 17:17:35 -0600

    session handling with cookies and jwt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import { PORT, SECRET_JWT_KEY } from './config.js';
 import { UserRepository } from './user-repository.js';
 
 const app = express();
@@ -7,8 +9,10 @@ const app = express();
 app.set('view engine', 'ejs');
 // middleware
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
+  // this is SSR server side rendering
   res.render('index');
 });
 
@@ -16,14 +20,25 @@ app.post('/login', async(req, res) => {
   const { username, password } = req.body;
   try {
     const user = await UserRepository.login({username, password});
-    res.send({ user });
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      SECRET_JWT_KEY,
+      { expiresIn: '1h' }
+    );
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // cookie can be only accessed in the server
+        secure: process.env.NODE_ENV === 'production', // only https access
+        sameSite: 'strict', // only in the same domain
+        maxAge: 1000 * 60 * 60 // valid 1 hour
+      })
+      .send({ user });
   } catch (error) {
     res.status(401).send({ error: error.message });
   }
 });
 app.post('/register', async(req, res) => {
   const { username, password } = req.body;
-
   try {
     const id = await UserRepository.create({username, password});
     res.send({ id });
@@ -35,7 +50,19 @@ app.post('/register', async(req, res) => {
 
 app.post('/logout', (req, res) => {});
 
-app.post('/protected', (req, res) => {});
+app.post('/protected', (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return res.status(403).send('access not authorized');
+  }
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY);
+    res.render('protected', data);
+  } catch (error) {
+    return res.status(401).send('access not authorized');
+  }
+  res.render('protected', { username: 'admin' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
