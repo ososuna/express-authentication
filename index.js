5a640530a69e46277b8058bbae0e10bae31ffdb8
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-11-05 16:01:52 -0600

    user repository

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import express from 'express';
-import { PORT } from './config'
+import { PORT } from './config.js'
+import { UserRepository } from './user-repository.js';
 
 const app = express();
+// middleware
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  response.send('Hello world');
+  res.send('Hello world!!!');
 });
+
+app.post('/login', (req, res) => {
+  res.json({user: 'user'});
+});
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const id = UserRepository.create({username, password});
+    res.send({ id });
+  } catch (error) {
+    // normally, it is not a good idea to send the error.message
+    res.status(400).send({ error: error.message });
+  }
+});
+
+app.post('/logout', (req, res) => {});
+
+app.post('/protected', (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
